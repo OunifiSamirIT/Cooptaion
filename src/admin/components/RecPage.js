b7a25c62f7bdb01478988651e053de2431349024
OunifiSamir <59454272+OunifiSamirIT@users.noreply.github.com>
2022-07-24 13:34:04 +0100

    v1.0

--- src/admin/components/RecPage.js
+++ src/admin/components/RecPage.js
@@ -1,13 +1,35 @@
 // import React from 'react' ;
 import './RecStyle.css'
+import Logo from "./ll.jpg"
 
 function RecPage() {
     return (
+        
         <div className="recPage">
             <div className="recNavBar">
-                <h4>acceuil</h4>
-                <h4>acceuil</h4>
-                <h4>acceuil</h4>
+<div className="imgContainer">
+<img src={Logo} alt="" />
+
+</div>
+                <nav>
+                    <ul>
+                        <li>
+                    <a href="http://">Acceuil</a>
+
+                        </li>
+                        <li>
+                    <a href="http://">Offre</a>
+                            
+                        </li>
+                        <li>
+                        <a href="http://">Room</a>
+
+                        </li>
+                    </ul>
+                    
+                </nav>
+
+                    <button className="logOutBtn">Log out</button>
             </div>
             <div className="recForm">
                 <h5>Recommendé par SAMIR OUNIFI</h5>
@@ -17,16 +39,24 @@ function RecPage() {
                 <input type="text" placeholder="telephone" />
                 <input type="text" placeholder="url linkedIn" />
                 <input type="submit" id="submitBtn" value="visualiser cv" />
+            
+                <footer>
+                {/* <p>copyright &copy;</p> */}
+            </footer>
             </div>
             <div className="recCv">
-                <div className="imageContainer"></div>
+                <div className="imageContainer">
+                    <img src="/images/85-modele-cv-exemplaire.jpg" alt="" />
+                    <div id="shadowBox"></div>
+                </div>
                 <div className="buttonContainer">
                     <input className="btnDec" type="submit" value="refuse" />
                     <input className="btnDec" type="submit" value="envoye" />
                 </div>
-
-
+                
+                
             </div>
+
         </div>
     )
 }
